c0844710d93e2a2bc99b08cd913142497a0d6be8
vhqr0 <zq_cmd@163.com>
2023-04-10 17:37:29 +0800

    socks5 outbox

--- proxy/connector/__init__.py
+++ proxy/connector/__init__.py
@@ -2,6 +2,7 @@
 from .base import Connector, ProxyConnector
 from .http import HTTPConnector
 from .null import NULLConnector
+from .socks5 import Socks5Connector
 from .tcp import TCPConnector
 from .trojan import TrojanConnector
 from .ws import WSConnector
--- proxy/outbox/__init__.py
+++ proxy/outbox/__init__.py
@@ -2,5 +2,6 @@
 from .base import Outbox, TLSCtxOutbox
 from .http import HTTPOutbox
 from .null import NULLOutbox
+from .socks5 import Socks5Outbox
 from .tcp import TCPOutbox
 from .trojan import TrojanOutbox
